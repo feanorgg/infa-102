d69d2aa099723c58afcb7c3aa13fcb11725363f7
Ivan Galkin <ivan.galkin5066@gmail.com>
2023-02-27 22:00:19 +0300

    Убрал коменты из 3 и начал делать задание от 28.01

---
+++ 28.02.2023/1/main.cpp
@@ -0,0 +1,17 @@
+// Галкин Иван Андреевич, 102
+// Задача к 4 семинару 2 семестра (Задания по теме 2_4.doc) - код для проверки выполнения задания 1 (myLib.h)
+
+#include <iostream>
+#include "myLib.h"
+
+using std::cin;
+using std::cout;
+using namespace MySpace;
+
+Array::length = 5;
+
+int main() {
+    Array arr([0, 1, 2, 3, 4]);
+
+    return 0;
+}
--- 28.02.2023/1/myLib.h
+++ 28.02.2023/1/myLib.h
@@ -1,8 +1,9 @@
 // Галкин Иван Андреевич, 102
-// Задача к 3 семинару 2 семестра (Задания по теме 2_3.doc) - файл библиотеки к заданию 2
+// Задача к 4 семинару 2 семестра (Задания по теме 2_4.doc) - файл библиотеки к заданию 1
 
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -14,9 +15,15 @@ namespace MySpace {
         private:
             double* data;
             static int length;
+            char* name;
 
         public:
             Array(double in = 0);
+            Array(double* arr);
+
+            Array(double in, char* _name);
+            Array(double* arr, char* _name);
+
             ~Array();
 
             static int Length();
@@ -36,17 +43,65 @@ namespace MySpace {
             friend Array operator* (const Array &arr, const double &x);
             friend Array operator* (const double &x, const Array &arr);
             friend ostream & operator<< (ostream &os, const Array &arr);
+            friend ostream & operator>> (ostream &os, const Array &arr);
+
+            operator double() {
+                double summ = 0;
+                for(int i = 0; i < length; i++) {
+                    summ += data[i]*data[i];
+                }
+
+                return sqrt(summ);
+            }
+
+            double& Array::operator[] (int i) {
+                return data[i];
+            }
+
+            void UpdateName(char* str);
     };
 
     Array::Array(double in) {
+        name = "Default";
         data = new double[length];
         for(int i = 0; i < length; i++) {
             data[i] = in;
         }
     }
 
+    Array::Array(double* arr) {
+        name = "Default";
+        data = new double[length];
+        for(int i = 0; i < length; i++) {
+            data[i] = arr[i];
+        }
+    }
+
+    Array::Array(double in, char* _name) {
+        int len = strlen(_name) + 1;
+        name = new char[len];
+        strncpy(name, _name, len);
+
+        data = new double[length];
+        for(int i = 0; i < length; i++) {
+            data[i] = in;
+        }
+    }
+
+    Array::Array(double* arr, char* _name) {
+        int len = strlen(_name) + 1;
+        name = new char[len];
+        strncpy(name, _name, len);
+
+        data = new double[length];
+        for(int i = 0; i < length; i++) {
+            data[i] = arr[i];
+        }
+    }
+
     Array::~Array() {
         delete [] data;
+        delete [] name;
     }
 
     int Array::Length() {
@@ -54,6 +109,10 @@ namespace MySpace {
     }
 
     Array::Array(const Array & arr2) {
+        int len = strlen(arr2.name) + 1;
+        name = new char[len];
+        strncpy(name, arr2.name, len);
+
         data = new double[length];
         for(int i = 0; i < length; i++) {
             data[i] = arr2.data[i];
@@ -97,5 +156,17 @@ namespace MySpace {
         os << "]";
 
         return os;
+    }
+
+    ostream & operator>> (ostream &os, const Array &arr) {
+        for(int i = 0; i < Array::Length(); i++) {
+            os >> arr.data[i];
+        }
+    }
+
+    void Array::UpdateName(char* str) {
+        int len = strlen(str) + 1;
+        name = new char[len];
+        strncpy(name, str, len);
     }
 }
